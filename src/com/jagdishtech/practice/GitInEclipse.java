8706ccd0cb8898bca3b6f8e6df7976c0ae6d1ec4
jagdish <mail2jagdishpatil@gmail.com>
2017-11-18 15:19:07 +0530

    Added a comment to the main method
    Change-Id: I946356d369d763e0f2e322e7bb0bf3a536a21f16
    Signed-off-by: jagdish <mail2jagdishpatil@gmail.com>

--- src/com/jagdishtech/practice/GitInEclipse.java
+++ src/com/jagdishtech/practice/GitInEclipse.java
@@ -1,7 +1,8 @@
 package com.jagdishtech.practice;
 
 public class GitInEclipse {
-
+  
+	// This is the main method
 	public static void main(String[] args) {
 		System.out.println("This is the first git clone project in Eclipse");
 	}
