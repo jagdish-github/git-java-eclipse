c5abfbfaddf5e9399ae871a6b49f00558efd57ed
Jagdish Patil <mail2jagdishpatil@gmail.com>
2017-11-18 16:38:12 +0530

    Changing ///////////

--- src/com/jagdishtech/practice/GitInEclipse.java
+++ src/com/jagdishtech/practice/GitInEclipse.java
@@ -2,7 +2,7 @@ package com.jagdishtech.practice;
 
 public class GitInEclipse {
   
-	// This is the main method*/
+	// This is the main method////////////////////
 	public static void main(String[] args) {
 		System.out.println("This is the first git clone project in Eclipse");
 	}
