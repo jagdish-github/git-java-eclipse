86151986958e4151ea9800fb52c8425a42dde0fb
Jagdish Patil <mail2jagdishpatil@gmail.com>
2017-11-19 13:08:28 +0530

    Adding git fetch comment
    Adding git fetch comment to demonstrate working of git fetch command

--- src/com/jagdishtech/practice/GitInEclipse.java
+++ src/com/jagdishtech/practice/GitInEclipse.java
@@ -1,6 +1,7 @@
 package com.jagdishtech.practice;
 
 public class GitInEclipse {
+	/* git fetch ......................................*/
   	// This is the main method
 	public static void main(String[] args) {
 		System.out.println("This is the first git clone project in Eclipse");
