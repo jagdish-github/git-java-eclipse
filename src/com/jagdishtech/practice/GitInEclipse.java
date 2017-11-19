dc58ae2830996b397ede56b49d7349d559129734
Jagdish Patil <mail2jagdishpatil@gmail.com>
2017-11-19 13:29:06 +0530

    Adding some missed statements
    Adding some missed statements

--- src/com/jagdishtech/practice/GitInEclipse.java
+++ src/com/jagdishtech/practice/GitInEclipse.java
@@ -5,6 +5,8 @@ public class GitInEclipse {
   	// This is the main method
 	public static void main(String[] args) {
 		System.out.println("This is the first git clone project in Eclipse");
+		System.out.println("Added to demonstrate resolve merge conflicts");
+		int a =10;
 	}
 
 }
