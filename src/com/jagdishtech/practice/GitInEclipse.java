401c1991b1d6a4cc93cbb932afc8412026606889
jagdish <mail2jagdishpatil@gmail.com>
2017-11-19 12:11:54 +0530

    Removing comment for git fectch demo

--- src/com/jagdishtech/practice/GitInEclipse.java
+++ src/com/jagdishtech/practice/GitInEclipse.java
@@ -1,8 +1,7 @@
 package com.jagdishtech.practice;
 
 public class GitInEclipse {
-  	// git fetch demo
-	// This is the main method
+  	// This is the main method
 	public static void main(String[] args) {
 		System.out.println("This is the first git clone project in Eclipse");
 	}
