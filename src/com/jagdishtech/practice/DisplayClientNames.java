ee0e6b9bf1c1ab5f00307acca267ce38bdb8de23
jagdish <mail2jagdishpatil@gmail.com>
2017-11-24 11:44:42 +0530

    ABSI-773:Adding file to display client names

---
+++ src/com/jagdishtech/practice/DisplayClientNames.java
@@ -0,0 +1,11 @@
+package com.jagdishtech.practice;
+
+public class DisplayClientNames {
+	
+	public void displayClientNames(){
+		System.out.println("Volkswagen");
+		System.out.println("BMW");
+		System.out.println("Ford");
+		System.out.println("FIAT");
+	}
+}
