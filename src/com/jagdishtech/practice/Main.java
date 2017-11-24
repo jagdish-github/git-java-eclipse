0ce9d296e2aabc62495dbeb89d90254b63c8667a
jagdish <mail2jagdishpatil@gmail.com>
2017-11-24 12:16:54 +0530

    ABSI-773: Main program to instantiate DisplayClientNames class

---
+++ src/com/jagdishtech/practice/Main.java
@@ -0,0 +1,10 @@
+package com.jagdishtech.practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("In main() method of Main.java class");
+		DisplayClientNames displayClientNames = new DisplayClientNames();
+		displayClientNames.displayClientNames();
+	}
+}
